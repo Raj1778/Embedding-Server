b11acc4063489a190f2f07f13b9d61f9d7687958
Raj1778 <rajmhr078@gmail.com>
2026-03-06 09:50:51 +0530

    Memory optimizations for deployment

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
+import torch
+import os
+
+
+torch.set_grad_enabled(False)
+
+
+torch.set_num_threads(1)
+os.environ["OMP_NUM_THREADS"] = "1"
 
 app = FastAPI()
 
 model = SentenceTransformer(
-    "all-MiniLM-L6-v2",
+    "paraphrase-MiniLM-L3-v2",
     device="cpu"
 )
+
+class EmbedRequest(BaseModel):
+    text: str
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
 @app.post("/embed")
-def embed(text: str):
-    embedding = model.encode(text, normalize_embeddings=True).tolist()
+def embed(req: EmbedRequest):
+    embedding = model.encode(
+        req.text,
+        normalize_embeddings=True,
+        convert_to_numpy=True,
+        show_progress_bar=False
+    ).tolist()
+
     return {"embedding": embedding}
